a588d91c0bc3919a558b33909e817dfa91e4dab4
ajishrose <rajish@purdue.edu>
2023-09-29 13:56:13 -0400

    9.1, 9.2: home page UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import logo from './BBLogoTransparent2.png';
 
+/* home page UI */
+function Home() {
+  return (
+    <section id="home">
+    </section>
+  )
+}
+
+/* set monthly goal page UI */
+function SetMonthlyGoal() {
+  return (
+    <h2>set monthly budget</h2>
+  )
+}
+
+/* category breakdown page UI */
+function CategoryBreakdown() {
+  return (
+    <h2>category breakdown</h2>
+  )
+}
+
+/* input daily spending UI */
+function InputDailySpending() {
+  return (
+    <h2>input daily spending</h2>
+  )
+}
+
+/* handle rendering of pages and options */
 function App() {
+  const [currentPage, setCurrentPage] = React.useState('home');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home/>;
+      case 'setMonthlyGoal':
+        return <SetMonthlyGoal/>;
+      case 'categoryBreakdown':
+        return <CategoryBreakdown/>;
+      case 'inputDailySpending':
+        return <InputDailySpending/>;
+      default:
+        return <Home/>;
+    }
+  };
+
+  const handleNavigation = (page) => {
+    setCurrentPage(page);
+  };
+
+  /* return home page structure */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <button className='nav-button' onClick={() => handleNavigation('home')}>Home</button>
+          </li>
+          <li>
+            <button className='nav-button' onClick={() => handleNavigation('setMonthlyGoal')}>Set Monthly Goal</button>
+          </li>
+          <li>
+            <button className='nav-button' onClick={() => handleNavigation('categoryBreakdown')}>Category Breakdown</button>
+          </li>
+          <li>
+            <button className='nav-button' onClick={() => handleNavigation('inputDailySpending')}>Input Daily Spending</button>
+          </li>
+        </ul>
+      </nav>
+      
+      <div class="box">
+        <img
+          src={logo} alt = ''
+        />
+
+        <main>{renderPage()}</main>
+      </div>
     </div>
   );
 }
