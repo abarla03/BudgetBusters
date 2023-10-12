82014943b63bfa48463ec4ff65be8854b97a0fa9
sarahps121@gmail.com <spushpa@purdue.edu>
2023-10-12 18:33:24 -0400

    Incorporated add. monthly goal logic

--- src/components/SetMonthlyGoal.js
+++ src/components/SetMonthlyGoal.js
@@ -15,6 +15,9 @@ function SetMonthlyGoal() {
     const [newCategory, setNewCategory] = useState('');
     const [createdCategories, setCreatedCategories] = useState([]);
     const [displayCreatedCategories, setDisplayCreatedCategories] = useState(false);
+    const [showAllCategories, setShowAllCategories] = useState(false);
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [allCategories, setAllCategories] = useState([]);
 
   /* function handling non-numeric values in budget goal field */
   const handleBudgetChange = (event) => {
@@ -34,8 +37,10 @@ function SetMonthlyGoal() {
   const handleCategoryClick = (category) => {
     if (selectedCategories.includes(category)) {
       setSelectedCategories(selectedCategories.filter((c) => c !== category));
+      setAllCategories(allCategories.filter((c) => c !== category));
     } else {
       setSelectedCategories([...selectedCategories, category]);
+      setAllCategories([...allCategories, category]);
     }
   };
 
@@ -44,22 +49,30 @@ function SetMonthlyGoal() {
     if (newCategory.trim() !== '') {
       const updatedCategories = [...createdCategories, newCategory];
       setCreatedCategories(updatedCategories);
+      setAllCategories([...allCategories, newCategory]);
       setNewCategory('');
     }
   };
 
   /* function handling user's ability to delete their created categories */
   const handleRemoveCategory = (categoryToRemove) => {
-    const updatedCategories = createdCategories.filter((category) => category !== categoryToRemove);
+    const updatedCategories = createdCategories.filter((c) => c !== categoryToRemove);
     setCreatedCategories(updatedCategories);
+    setAllCategories(allCategories.filter((c) => c !== categoryToRemove));
   };
 
   const handleSubmit = () => {
     // add logic to submit here
+    setFormSubmitted(true);
+    setShowAllCategories(true);
   };
 
   return (
     <div>
+       {formSubmitted ? (
+      <SelectedCategoriesPage selectedCategories={allCategories} />
+    ) : (
+      <>
       <h3>Set Monthly Budget:</h3>
       <div className="input-container">
         <input className='user-input-field'
@@ -115,14 +128,84 @@ function SetMonthlyGoal() {
       </div>
 
       <button
-        className="submit-button"
+        className="next-button"
         type="submit"
         onClick={handleSubmit}
         disabled={error !== ''}
+      >
+        Next
+      </button>
+      </>
+    )}
+    </div>
+  );
+}
+
+/* set monthly goal pt.2: function handling color coding categories */
+function SelectedCategoriesPage({ selectedCategories }) {
+  const [colorOptions, setColorOptions] = useState({});
+  const hexColorOptions = {
+    Red: '#ff5c70',
+    Orange: '#ffb267',
+    Yellow: '#e9fc87',
+    Green: '#85d67a',
+    Teal: '#47c8a6',
+    Blue: '#5c9dc5',
+    Violet: '#6d5eb0',
+    Purple: '#af7abd',
+    Pink: '#e36498',
+    Grey: '#ccc'
+  };
+
+  const handleColorChange = (category, color) => {
+    setColorOptions((prevColorOptions) => ({
+      ...prevColorOptions,
+      [category]: color,
+    }));
+  };
+
+  return (
+    <div>
+      <h3>Selected Categories:</h3>
+      <ul>
+        {selectedCategories.map((category) => (
+          <li key={category}>
+            <button className="category-button"
+              id={`button-${category}`} 
+              style={{ backgroundColor: colorOptions[category] || '#ccc' }}
+            >
+              {category}
+            </button>
+            <select
+              onChange={(e) => handleColorChange(category, e.target.value)}
+              value={colorOptions[category] || ''}
+            >
+              <option value="">Choose Color</option>
+              {Object.entries(hexColorOptions).map(([colorName, hexCode]) => (
+                <option key={hexCode} value={hexCode}>
+                  {colorName}
+                </option>
+              ))}
+            </select>
+          </li>
+        ))}
+      </ul>
+      <button
+        className="back-button"
+        type="submit"
+      >
+        Back
+      </button>
+      <button
+        className="next-button"
+        type="submit"
       >
         Submit
       </button>
     </div>
   );
 }
+
+
+
 export default SetMonthlyGoal;
