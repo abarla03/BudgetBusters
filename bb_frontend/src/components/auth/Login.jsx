0ae08cf7346e020be382987dcf492c322ee35d73
sarahps121@gmail.com <spushpa@purdue.edu>
2023-11-01 11:01:59 -0400

    implemented register/login UI messages based on user input

--- bb_frontend/src/components/auth/Login.jsx
+++ bb_frontend/src/components/auth/Login.jsx
@@ -14,7 +14,7 @@ const Login = (props) => {
     const navigate = useNavigate();
     const [email, setEmail ] = useState('');
     const [password, setPassword] = useState('');
-    const [ loginSuccess, setLoginSuccess ] = useState(false);
+    const [displayMessage, setDisplayMessage ] = useState('');
     const [isSent, setIsSent] = useState(false);
 
     const loginSubmit = (e) => {
@@ -23,16 +23,58 @@ const Login = (props) => {
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 console.log(userCredential);
+                //const successMessage = "You have successfully logged in!";
+                //showMessage(successMessage);
+                // maybe show a success message on home, like 'successfully logged in as ''
+
                 // successful login
                 // direct user to home page
-                //history.push('/home');
-                setLoginSuccess(true);
                 navigate('/home');
             })
             .catch((error) => {
-                console.log(error);
+                console.log(`showing the error: ${error}`);
+                showMessage(error);
             })
     }
+
+    /* function to display appropriate UI messages based on user input */
+    const showMessage = (error) => {
+        if (error && error.message) {
+            // handle firebase errors
+            const match = error.message.match(/\(([^)]+)\)/);
+
+            switch (error.message.match(/\(([^)]+)\)/)[1]) {
+                // diff firebase errors:
+                case 'auth/invalid-email':
+                    setDisplayMessage("Please enter a valid email!");
+                    break;
+
+                case 'auth/invalid-login-credentials':
+                    setDisplayMessage("Please enter the correct credentials");
+                    break;
+
+                // case 'auth/missing-email':
+                //     break;
+
+                case 'auth/missing-password':
+                    setDisplayMessage("Please enter your password!");
+                    break;
+
+                default:
+                    setDisplayMessage("An error occurred. Please try again.");
+            }
+        } else {
+            // non-firebase errors/message
+            setDisplayMessage("An error occurred. Please try again.");
+            // successful login
+            // if (error === "You have successfully logged in!") {
+            //     setDisplayMessage(error);
+            // } else {
+            //     setDisplayMessage("An error occurred. Please try again.");
+            // }
+        }
+    };
+
 
     const handleResetPassword = () => {
         console.log("clicked on reset password link");
@@ -59,8 +101,9 @@ const Login = (props) => {
                 <label htmlFor="password">Password</label>
                 <input value={password} onChange={(e) => setPassword(e.target.value)}type="password" placeholder="enter password" id="password" name="password" />
 
-                <button type="submit">Log In</button>
+                <button type="submit" onClick={loginSubmit}>Log In</button>
 
+                {<p>{displayMessage}</p>}
             </form>
             <button className="link-btn" onClick={() => navigate('/register')}> Don't have an account? Register here.</button>
             <button className="link-btn" onClick = { handleResetPassword }>Reset Password</button>
