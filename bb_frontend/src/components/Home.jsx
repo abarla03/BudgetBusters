b83fb4f2854320a09dab006e01346567c179a1c4
ajishrose <rajish@purdue.edu>
2023-11-28 11:42:44 -0500

    home page construction p1

--- bb_frontend/src/components/Home.jsx
+++ bb_frontend/src/components/Home.jsx
@@ -5,14 +5,45 @@ import logo from '../BBLogo.png';
 import { Route, Routes, Link  } from 'react-router-dom';
 import SetMonthlyGoal from './SetMonthlyGoal'; // Import your components
 import CategoryBreakdown from './CategoryBreakdown'; // Import your components
-import InputDailySpending from './InputDailySpending'; // Import your components
+import InputDailySpending from './InputDailySpending';
+import {useState, useEffect} from "react";
+import Register from "./auth/Register";
+import {auth} from "../firebase"; // Import your components
 
 function Home() {
-    console.log("Home component is rendering.");
+    const user = auth.currentUser;
+    const userEmail = user ? user.email : "";
+
+    // return (
+    //     <div className="App">
+    //         <h3>Welcome to the Home Page!</h3>
+    //     </div>
+    // );
+    const [showWelcomeMessage, setShowWelcomeMessage] = useState(false);
+
+    useEffect(() => {
+        // Check if the user has just registered
+        const justRegistered = localStorage.getItem('justRegistered');
+
+        if (justRegistered) {
+            // If the user just registered, show the welcome message
+            setShowWelcomeMessage(true);
+
+            // Clear the flag to prevent showing the welcome message again
+            localStorage.removeItem('justRegistered');
+        }
+    }, []);
 
     return (
         <div className="App">
-            <h3>Welcome to the Home Page!</h3>
+            {showWelcomeMessage ? (
+                <div>
+                    <h3>Welcome to the Home Page, {userEmail}!</h3>
+                    <p>Here are instructions for how to use the app.</p>
+                </div>
+            ) : (
+                <h3>Welcome to the Home Page!</h3>
+            )}
         </div>
     );
 }
--- bb_frontend/src/components/auth/Register.jsx
+++ bb_frontend/src/components/auth/Register.jsx
@@ -6,7 +6,9 @@ import logo from "../../BBLogo.png";
 import { useNavigate } from "react-router-dom";
 import GoogleSignInButton from '../GoogleSignInButton';
 import GoogleSignUpButton from "../GoogleSignUpButton";
+import Home from "../Home";
 import * as currentUser from "firebase/auth";
+
 const Register = (props) => {
     const navigate = useNavigate();
     const [name, setName ] = useState('');
@@ -21,6 +23,8 @@ const Register = (props) => {
         email: '',
         phoneNumber: ''
     })
+
+    const [isUserRegistered, setIsUserRegistered] = useState(false);
 
     const signInWithGoogleHandler = () => {
         signInWithGoogle()
@@ -119,6 +123,8 @@ const Register = (props) => {
                 const successMessage = "You have successfully created an account!";
                 console.log(successMessage);
                 window.alert("You have successfully created an account!");
+                // localStorage.setItem('justRegistered', 'true');
+                setIsUserRegistered(true);
                 navigate('/login')
                 //showMessage(successMessage);
                 //showMessage("You have successfully created an account!");
@@ -158,4 +164,4 @@ const Register = (props) => {
     )
 }
 
-export default Register
+export default Register;
