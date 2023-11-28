e8c2b92acdf035e913ffcff7c16df8717ef604c8
abarla03 <abarla@purdue.edu>
2023-11-27 20:18:39 -0500

    set the "Welcome to" page

--- bb_frontend/src/components/Home.jsx
+++ bb_frontend/src/components/Home.jsx
@@ -1,5 +1,8 @@
 /****************** modified to match App.cs ***************************** */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useLocation } from 'react-router-dom';
+import { onAuthStateChanged} from "firebase/auth";
+import { auth } from "../firebase";
 import '../App.css';
 import logo from '../BBLogo.png';
 import { Route, Routes, Link  } from 'react-router-dom';
@@ -9,10 +12,28 @@ import InputDailySpending from './InputDailySpending'; // Import your components
 
 function Home() {
     console.log("Home component is rendering.");
+    const location = useLocation();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
 
     return (
         <div className="App">
-            <h3>Welcome to the Home Page!</h3>
+            { user ? (
+                <h3>Welcome to the Home Page, {user.displayName}!</h3>
+            ) : (
+                <h3>Welcome to the Home Page!</h3>
+            )}
         </div>
     );
 }
