4fe790afddab4d152959747d451ba2357ffb55fd
“angela-joseph-r” <josep103@purdue.edu>
2023-11-30 14:31:24 -0500

    current sprint us 6 AC satified

--- bb_frontend/src/components/CategoryBreakdown.js
+++ bb_frontend/src/components/CategoryBreakdown.js
@@ -4,7 +4,6 @@ import {auth} from "../firebase";
 import {get} from "./ApiClient";
 function CategoryBreakdown() {
 
-    // get user
     const user = auth.currentUser;
     const userEmail = user ? user.email : "";
 
@@ -50,36 +49,7 @@ function CategoryBreakdown() {
         setInputDailyUpdated(false);
     }, [userEmail, inputDailyUpdated]);
 
-    // const data = [
-    //     { category: "Rent", total_spending: 150 },
-    //     { category: "Food", total_spending: 100 },
-    //     { category: "Gym", total_spending: 50 },
-    //     { category: "Yet to Spend", total_spending: 200 },
-    // ];
-    //
-    // const colors = ["#8884d8", "#82ca9d", "#FFBB28", "#808080"];
-
-
-    // // hard coded category breakdown data - pull from backend
-// const categories = budgetGoalObj?
-// const colors = ["#8884d8", "#82ca9d", "#FFBB28"];
-// const categoryCount = [150, 100, 50];
-//
-// // creating the Yet to Spend category
-// budgetLimit = 500;
-// const sum = categoryCount.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-// categories.push("Yet to Spend");
-// const yetToSpendCount = budgetLimit - sum;
-// categoryCount.push(yetToSpendCount);
-// colors.push("#808080");
-//
-// // create bar_data for display
-// const bar_data = [];
-// categories.map((category, count) => {
-//   bar_data.push({ name: category, total_spending: categoryCount[count]});
-// });
-// console.log(bar_data);
-
+    /* category data from backend */
     let colors;
     let categories;
     let categoryCount;
@@ -106,6 +76,27 @@ function CategoryBreakdown() {
         categories.map((category, count) => {
           bar_data.push({ name: category, total_spending: categoryCount[count]});
         });
+    } else {
+        colors = budgetGoalObj.colors ? [...budgetGoalObj.colors] : [];
+        categories = budgetGoalObj.allCategories ? [...budgetGoalObj.allCategories] : [];
+        categoryCount = inputDailyObj.categoryCount ? [...inputDailyObj.categoryCount] : [];
+        budgetLimit = budgetGoalObj.monthlyBudget;
+
+        let sum = 0;
+        if ((inputDailyObj?.cumulativeDailySpending)?.length > 0) {
+            sum = inputDailyObj?.cumulativeDailySpending[(inputDailyObj?.cumulativeDailySpending)?.length - 1];
+        }
+
+        const totalRemainingCount = parseInt(budgetLimit) - sum;
+        categories.push("Total Remaining");
+        categoryCount.push(totalRemainingCount);
+        colors.push("#808080");
+
+        bar_data = [];
+        categories.map((category, count) => {
+            // displaying only the categories on the bar graph
+            bar_data.push({ name: category, total_spending: 0});
+        });
     }
 
     /* Bar Chart components */
@@ -116,8 +107,9 @@ function CategoryBreakdown() {
                     <h1 className="text-heading">Monthly Category Breakdown</h1>
                     <BarChart width={600} height={600} data={bar_data}>
                         <Bar dataKey="total_spending" fill="green" />
+                        {/*<Bar dataKey="total_spending" fill={(entry, index) => colors[index] || "green"} />*/}
                         <CartesianGrid stroke="#ccc" />
-                        <XAxis dataKey="category" />
+                        <XAxis dataKey="name" />
                         <YAxis />
                     </BarChart>
                 </div>
@@ -128,8 +120,13 @@ function CategoryBreakdown() {
     return (
         <div>
             <h2>Category Breakdown</h2>
-            <MyBarChart/>
-            {/* Add content and logic for category breakdown */}
+            {
+                !budgetGoalObj?.colors ? (
+                    <h3>Please set your monthly goal and add purchases to view your progress.</h3>
+                ) : (
+                    <MyBarChart />
+                )
+            }
         </div>
     );
 }
