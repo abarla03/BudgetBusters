b9eedfb68d871c4522bad3153b44b4b41f926bda
“angela-joseph-r” <josep103@purdue.edu>
2023-12-01 09:44:49 -0500

    running notifs from Test_notifs.js

---
+++ bb_frontend/src/components/Test_notifs.js
@@ -0,0 +1,7 @@
+const sendMonthlyNotif = require("../../../functions/index"); // Replace './filename' with the actual file path
+const sendMonthlyGoalsEmail = require("../../../functions/index");
+
+
+sendMonthlyNotif();
+sendMonthlyGoalsEmail();
+
